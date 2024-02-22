eb96345018454fc38ed141b1e5acbaae3580aa07
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-02-22 17:20:36 -0300

    actualizar index.js/package.json - Crear server, res.writeHead, res.end() y cambiar scripts ['test' (x) -> 'start' + 'dev']

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if(req.url === '/'){
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end('<h1>Home</h1>');
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen( PORT, ()=>console.log(`Server running on port ${PORT}`) );
