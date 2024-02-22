f39696ffe066085d4e24a186aec124572364f8e4
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-02-22 12:34:16 -0300

    actualizar fs_demo.js - Ejemplo writeFile (with overwrite)

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,7 +2,22 @@ const fs = require('fs');
 const path = require('path')
 
 //Create folder
-fs.mkdir( path.join(__dirname,'/test'), {}, err => {
+/* fs.mkdir( path.join(__dirname,'/test'), {}, err => {
     if(err) throw err;
     console.log('Folder Created');
+}) */
+
+//Create and write file
+fs.writeFile( path.join(__dirname, '/test','hello.txt'),
+'Hello World!',
+err => {
+    if(err) throw err;
+    console.log('File written to...');
+})
+//Overwrite...
+fs.writeFile( path.join(__dirname, '/test','hello.txt'),
+'I Love NodeJS!',
+err => {
+    if(err) throw err;
+    console.log('File written to...');
 })
