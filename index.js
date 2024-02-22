2368d69ef01253399eb254b586eb12e5aac24dca
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-02-22 18:35:42 -0300

    Nuevos archivos index.html, about.html - index.js envia contenido html según url

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const fs = require('fs');
 const server = http.createServer((req,res)=>{
     console.log(req.url);
     if(req.url === '/'){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.end('<h1>Home</h1>');
+        console.log('home');
+        fs.readFile( path.join(__dirname, '/public', 'index.html'), (err,content) => {
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.end(content);
+        } )
+    }
+    if(req.url === '/about'){
+        fs.readFile( path.join(__dirname, '/public', 'about.html'), (err,content)=>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.end(content);
+        } )
     }
 });
 
