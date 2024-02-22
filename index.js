b30c8bed0284dae729986762be0eaff0eeeb39c6
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-02-22 16:45:13 -0300

    nuevo logger.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const person = require('./person')
 console.log(person);
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+logger.on( 'message', (data) => console.log('LISTENER: ',data) )
+
+logger.log('Hello World')
